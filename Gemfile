0812fbb2b2dd69e5df743741854152bdbd99edc4
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-05-20 04:53:22 +0200

    Beautiful mister coctail

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'bootstrap-sass', '~> 3.4.1'
 gem 'carrierwave', '~> 1.2'
 gem 'cloudinary', '~> 1.9.1'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'faker'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'nokogiri'
 gem 'select2_simple_form', github: 'lndl/select2_simple_form', tag: '0.7.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Ingredient.destroy_all
-#Cocktail.destroy_all
-#Dose.destroy_all
+Review.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
 require 'open-uri'
 require 'json'
 require 'nokogiri'
+require 'faker'
 =begin
 
 response = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
@@ -34,23 +37,38 @@ base_url = 'https://www.thecocktaildb.com/browse.php?s='
 
 response = open('https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json').read
 cocktails = JSON.parse(response)
-cocktails.each do |cocktail|
+cocktails.each_with_index do |cocktail, cocktail_index|
   cocktail_db = Cocktail.new(name: cocktail['name'], preparation: cocktail['preparation'])
   doc = Nokogiri::HTML(open("#{base_url}#{cocktail['name'].downcase.tr(' ', '+')}").read)
   cocktail_db.image_url = doc.search('.col-sm-3 a img').first.attribute('src').value unless doc.search('.col-sm-3 a img').empty?
-
   cocktail_db.save
-  cocktail['ingredients'].each do |dose|
-    ingredient_db = Ingredient.new(name: dose['ingredient'])
-    ingredient_db.save
-    dose = Dose.new(description: "#{dose['amount']} #{dose['unit']}")
-    dose.cocktail = cocktail_db
-    dose.ingredient = ingredient_db
-    dose.save
+
+  [*3..5].sample.times do
+    content = Faker::Lorem.paragraph_by_chars([*56..256].sample, false)
+    content += Faker::SlackEmoji.people
+    review = Review.new(content: content, rating: [*1..5].sample)
+    review.cocktail = cocktail_db
+    review.save
+  end
+
+  cocktail['ingredients'].each_with_index do |dose, dose_index|
+    if dose.key?('ingredient')
+      ingredient_db = Ingredient.new(name: dose['ingredient'])
+      ingredient_db.save
+      ingredient_db = Ingredient.find_by(name: dose['ingredient']) unless ingredient_db.save
+      dose = Dose.new(description: "#{dose['amount']} #{dose['unit']} of #{dose['label'] || dose['ingredient']}")
+      dose.cocktail = cocktail_db
+      dose.ingredient = ingredient_db
+      p dose
+      dose.save
+    elsif dose.key?('special')
+      special_ingredient = Ingredient.new(name: "Special_#{cocktail_index + 1}_#{dose_index + 1}")
+      special_ingredient.save(validate: false)
+      dose = Dose.new(description: dose['special'])
+      dose.cocktail = cocktail_db
+      dose.ingredient = Ingredient.find_by(name: "Special_#{cocktail_index + 1}_#{dose_index + 1}")
+      p dose
+      dose.save
+    end
   end
 end
-
-Review.create!(cocktail_id: 1, content: "Very good", rating: 4)
-Review.create!(cocktail_id: 1, content: "Bad", rating: 1)
-Review.create!(cocktail_id: 1, content: "Excellent", rating: 5)
-
